7bfb4225a75e0b7cfe96d8f0eb0a63dea55feefa
asamarin <asamarin@neuro.net>
2024-11-08 12:36:01 +0300

    initial commit

--- src/run.sh
+++ src/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 python main.py &
-celery -A tasks.celery worker -f log/celery.log -l info &
-celery -A tasks.celery beat -f log/celery.log -l info
+celery -A tasks.celery worker -f log/celery.log -l warning &
+celery -A tasks.celery beat -f log/celery.log -l warning
