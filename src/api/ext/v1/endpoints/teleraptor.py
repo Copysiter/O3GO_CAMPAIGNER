272b632b520b1ac8263b8f57014aa81d6353f7a7
Александр Самарин <a.samarin@atol.ru>
2025-09-05 20:52:14 +0300

    Teleraptor 404 -> []

--- src/api/ext/v1/endpoints/teleraptor.py
+++ src/api/ext/v1/endpoints/teleraptor.py
@@ -43,7 +43,7 @@ async def proxy_get_next(
                 'text_sms': message.get('text'), 'error': '0'
             }]
     except HTTPException as e:
-        raise e
+        return []
     except Exception as e:
         await session.rollback()
         raise HTTPException(
