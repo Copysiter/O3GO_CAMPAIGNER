292dbf4fc9b2e0fa7fae34d886c7b95efc9d9711
Александр Самарин <a.samarin@atol.ru>
2026-07-09 00:54:29 +0300

    add clicker step

--- src/core/config.py
+++ src/core/config.py
@@ -138,7 +138,7 @@ class Settings(BaseSettings):
     AI_OPENROUTER_BATCH_SIZE: int = Field(10, env="AI_OPENROUTER_BATCH_SIZE")
 
     LINK_SHORTENER_URL: str = Field(
-        "http://link.clicker.ru/api/v1/shorten", env="LINK_SHORTENER_URL"
+        "http://link.o3go.ru/api/v1/shorten", env="LINK_SHORTENER_URL"
     )
     LINK_CLICKER_URL: str = Field(
         "https://link.o3go.ru/api/requests/create", env="LINK_CLICKER_URL"
